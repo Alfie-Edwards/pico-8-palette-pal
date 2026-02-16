311f163266c0f3bcc08dfc4b362db28ad60d22d5
Alfie Edwards <alfie-edwards@hotmail.com>
2026-02-16 21:04:38 +0000

    Improve corner dragging behaviour

--- web/utils.js
+++ web/utils.js
@@ -1,17 +1,20 @@
 export function onDrag(element, handler) {
-    element.addEventListener("pointermove", (e) => {
+    const listener = (e) => {
         if (e.buttons & 1) {
             return handler(e);
         }
-    });
+    }
 
     element.addEventListener("pointerdown", (e) => {
         console.log("grab", e.target)
         element.setPointerCapture(e.pointerId);
+        listener(e);
+        element.addEventListener("pointermove", listener);
     });
 
     element.addEventListener("pointerup", (e) => {
         console.log("release", e.target)
         try { element.releasePointerCapture(e.pointerId); } catch (err) {}
+        try { element.removeEventListener("pointermove", listener); } catch (err) {};
     });
 }
